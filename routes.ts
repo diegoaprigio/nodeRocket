740317eb30eba11da2b4b50b8a34ef4c453c6df6
Dyonatan Celestino <dyonatancelestino@gmail.com>
2021-05-07 15:53:29 -0300

    ff

---
+++ routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "./src/repositories/SettingsRepository";
+
+const routes = Router;
+
+routes.post("/settings", async (request, response) => {
+    const settingsRepository = getCustomRepository(SettingsRepository)
+
+    const settings = settingsRepository.create({
+        chat,
+        username
+    })
+
+    await settingsRepository.save(settings)
+})
+
+export { routes };
